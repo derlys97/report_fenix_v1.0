bd9e1b32676c7abd7d015569a600cb408e487beb
derlys97 <138208425+derlys97@users.noreply.github.com>
2025-10-27 11:08:16 -0300

    Add files via upload

---
+++ usuarios.js
@@ -0,0 +1,8 @@
+// usuarios.js
+const usuarios = [
+  { usuario: "admin", password: "Martel147", rol: "admin" },
+  { usuario: "raguayo@martel.com.py", password: "Martel147", rol: "Gerencia" },
+  { usuario: "jsabas@martel.com.py", password: "Martel147", rol: "B2C" },
+  { usuario: "liz.quintana@martel.com.py", password: "Martel147", rol: "B2B" },
+  { usuario: "ecomuser", password: "1234", rol: "ecom" }
+];
