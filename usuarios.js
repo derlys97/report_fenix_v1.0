3782cea329bace8d5295e0b8ac56310d6fe4a3d1
derlys97 <138208425+derlys97@users.noreply.github.com>
2025-10-27 15:45:35 -0300

    Update usuarios.js

--- usuarios.js
+++ usuarios.js
@@ -2,7 +2,13 @@
 const usuarios = [
   { usuario: "admin", password: "Martel147", rol: "admin" },
   { usuario: "raguayo@martel.com.py", password: "Martel147", rol: "Gerencia" },
+  { usuario: "mpena@martel.com.py", password: "Martel147", rol: "Gerencia" },
+  { usuario: "ggonzalez@martel.com.py", password: "Martel147", rol: "Gerencia" },
+  { usuario: "garanda@martel.com.py", password: "Martel147", rol: "Gerencia" },
+  { usuario: "agarcia@martel.com.py", password: "Martel147", rol: "Gerencia" },
+  { usuario: "derlys.almiron@martel.com.py", password: "Martel147", rol: "Gerencia" },
   { usuario: "jsabas@martel.com.py", password: "Martel147", rol: "B2C" },
   { usuario: "liz.quintana@martel.com.py", password: "Martel147", rol: "B2B" },
   { usuario: "ecomuser", password: "1234", rol: "ecom" }
 ];
+
